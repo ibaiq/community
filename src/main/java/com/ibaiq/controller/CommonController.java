3ff8b6f34065c60b08e2769afebd46b9103d3c58
ibaiq <416322112@qq.com>
2021-12-20 18:45:57 +0800

    移除判断是否有权限进入后台的接口

---
+++ src/main/java/com/ibaiq/controller/CommonController.java
@@ -0,0 +1,13 @@
+package com.ibaiq.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 十三
+ */
+@RestController
+public class CommonController extends BaseController {
+
+
+
+}
